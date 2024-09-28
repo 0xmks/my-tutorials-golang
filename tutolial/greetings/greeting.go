49b4ed212352c1541908354a15a20a9e8622f8a7
0xmks <17310701+0xmks@users.noreply.github.com>
2024-09-28 23:38:35 +0900

    fix: func Hellos

--- tutolial/greetings/greeting.go
+++ tutolial/greetings/greeting.go
@@ -16,6 +16,20 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+// Hellos returns a map that associates each of the named people with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
 // ranndomFormat returns on of set of greeting messages. the returnd message is selected at random.
 func ranndomFormat() string {
 	formats := []string{
